52c889975db1c434dc96280e71a44e5c602d6b80
arcvin <arcvin.corp@gmail.com>
2022-09-28 20:53:52 +0530

    Partially implemented issue #2

--- src/js/controller.js
+++ src/js/controller.js
@@ -5,14 +5,6 @@ import 'regenerator-runtime/runtime';
 
 const recipeContainer = document.querySelector('.recipe');
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
@@ -31,11 +23,12 @@ const controlRecipes = async function () {
     // Render the recipe
     recipeView.render(model.state.recipe);
   } catch (error) {
-    console.error(error);
+    recipeView.renderError(error);
   }
 };
 
-// Event listeners
-['hashchange', 'load'].forEach(event =>
-  window.addEventListener(event, controlRecipes)
-);
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
+
+init();
