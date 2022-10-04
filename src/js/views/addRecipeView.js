4cfcb55c0edd75fa07973a09837c8a28f39924b4
arcvin <arcvin.crop@gmail.com>
2022-10-04 08:54:57 +0530

    WIP

---
+++ src/js/views/addRecipeView.js
@@ -0,0 +1,33 @@
+import View from './view.js';
+import icons from 'url:../../img/icons.svg';
+
+class AddRecipeView extends View {
+  _parentElement = document.querySelector('.upload');
+  _window = document.querySelector('.add-recipe-window');
+  _overlay = document.querySelector('.overlay');
+  _btnOpen = document.querySelector('.nav__btn--add-recipe');
+  _btnClose = document.querySelector('.btn--close-modal');
+
+  constructor() {
+    super();
+    this._addHandlerShowWindow();
+    this._addHandlerCloseWindow();
+  }
+
+  _toggleModal() {
+    this._overlay.classList.toggle('hidden');
+    this._window.classList.toggle('hidden');
+  }
+
+  _addHandlerShowWindow() {
+    this._btnOpen.addEventListener('click', this._toggleModal.bind(this));
+  }
+
+  _addHandlerCloseWindow() {
+    this._btnClose.addEventListener('click', this._toggleModal.bind(this));
+  }
+
+  _generateMarkup() {}
+}
+
+export default new AddRecipeView();
